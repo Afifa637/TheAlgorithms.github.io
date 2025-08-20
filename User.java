9d79c6f84106c136c93c19d168cc4d0bc56bd20d
Afifa Sultana <afifasultana637@gmail.com>
2025-08-20 16:31:41 +0600

    Added User.java with getters, setters, constructors

---
+++ User.java
@@ -0,0 +1,32 @@
+public class User {
+    private String name;
+    private int age;
+
+    // Constructor
+    public User(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    // Getters
+    public String getName()
+    { 
+        return name; 
+    }
+
+    public int getAge()
+    { 
+        return age; 
+    }
+
+    // Setters
+    public void setName(String name)
+    { 
+        this.name = name; 
+    }
+
+    public void setAge(int age)
+    { 
+        this.age = age; 
+    }
+}
